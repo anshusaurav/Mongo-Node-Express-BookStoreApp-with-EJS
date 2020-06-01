8f212f7352f60171be3dfe6eb7143aca6e61c341
anshusaurav <anshu.saurav@gmail.com>
2020-06-01 16:09:57 +0530

    Address-linked with checkout

--- routes/addresses.js
+++ routes/addresses.js
@@ -10,14 +10,12 @@ router.get('/', async(req, res, next) =>{
     try{
         var addresses = await Address.find({user: id});
         
-        console.log('Please');
         res.render('addresses', {addresses});
     } catch(error) {
         return next(error);
     }
 });
 router.get('/add', (req, res, next) =>{
-    console.log('new address');
     res.render('newAddress');
 });
 router.post('/add', async(req, res, next) =>{
@@ -26,7 +24,7 @@ router.post('/add', async(req, res, next) =>{
     try{
         var address = await Address.create(req.body);
         var user = await User.findByIdAndUpdate(id, {$addToSet: {addresses: address.id}});
-        console.log(address, user);
+        // console.log(address, user);
         res.redirect('/addresses');
     }catch(error){
 
@@ -43,7 +41,21 @@ router.get('/:id/edit', async(req, res, next) =>{
 
     }
 });
+router.get('/onthego', (req, res, next) =>{
+    res.render('onthego');
+});
+router.post('/onthego', async(req, res, next) =>{
+    var id = req.session.userId;
+    req.body.user = id;
+    try{
+        var address = await Address.create(req.body);
+        var user = await User.findByIdAndUpdate(id, {$addToSet: {addresses: address.id}});
+        // console.log(address, user);
+        res.redirect('/addresses/cartaddress');
+    }catch(error){
 
+    }
+});
 router.post('/:id/edit', async(req, res, next) =>{
     var id = req.session.userId;
     req.body.user = id;
@@ -62,7 +74,11 @@ router.get('/:id/delete', async(req, res, next) =>{
     var addrId = req.params.id;
     try{
         var address = await Address.findByIdAndDelete(addrId);
-        var user = await User.findByIdAndUpdate(id, {$pull: {addresses: address.id}});
+        var user = await User.findByIdAndUpdate(id, {$pull: {addresses: addrId}});
+        if(user.hasDefaultAddress && addrId == user.defaultAddress) {
+            user = await User.findByIdAndUpdate(id,{$set:{hasDefaultAddress: false}} );
+            user = await User.findByIdAndUpdate(id,{$unset:{defaultAddress: 1}} );
+        }
         res.redirect('/addresses');
     }catch(error){
 
@@ -78,4 +94,27 @@ router.get('/:id/makedefault', async(req, res, next) =>{
 
     }
 });
+router.get('/cartaddress', async(req, res, next) =>{
+    var id = req.session.userId;
+    try{
+        var addresses = await Address.find({user: id});
+        
+        res.render('cartAddress', {addresses});
+    } catch(error) {
+        return next(error);
+    }
+})
+
+
+router.post('/cartaddress', async(req, res, next) =>{
+    //checkout here
+    var id = req.session.userId;
+    try{
+        var addresses = await Address.find({user: id});
+        
+        res.render('cartAddress', {addresses});
+    } catch(error) {
+        return next(error);
+    }
+})
 module.exports = router;
--- routes/search.js
+++ routes/search.js
@@ -8,32 +8,32 @@ router.post('/', async(req, res, next) =>{
     var query = req.body.searchQuery;
     console.log(query);
     
-        // Book.createIndexes({title: 1, author: 1}, {unique: true});
-        // Book.createIndexes({ title: 'text', author: 'text'});
-        Book.find(
-            { $text : { $search : query } }, 
-            { score : { $meta: "textScore" } }
-        )
-        .sort({ score : { $meta : 'textScore' } })
-        .exec(function(err, books) {
-            console.log(books);
-            books.forEach(book =>{
-                var sum = 0;
-                var cnt = 0;
-                book.ratings.forEach(elem =>{
-                    sum += elem.rating;
-                    cnt++;
-                })    
-                if(cnt > 0 ) {
-                    book.isRated = true;
-                    book.averageRating = Math.round(sum/cnt);
-                }
-                else{
-                    book.isRated = false;
-                }
-            })
-            res.render('searchResults',{books, query});
-        });
+    // Book.createIndexes({title: 1, author: 1}, {unique: true});
+    // Book.createIndexes({ title: 'text', author: 'text'});
+    Book.find(
+        { $text : { $search : query } }, 
+        { score : { $meta: "textScore" } }
+    )
+    .sort({ score : { $meta : 'textScore' } })
+    .exec(function(err, books) {
+        console.log(books);
+        books.forEach(book =>{
+            var sum = 0;
+            var cnt = 0;
+            book.ratings.forEach(elem =>{
+                sum += elem.rating;
+                cnt++;
+            })    
+            if(cnt > 0 ) {
+                book.isRated = true;
+                book.averageRating = Math.round(sum/cnt);
+            }
+            else{
+                book.isRated = false;
+            }
+        })
+        res.render('searchResults',{books, query});
+    });
         
     
 });
