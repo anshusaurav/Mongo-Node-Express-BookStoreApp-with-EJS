06fd64334d96ba11957ed783919fef6a9a84d1a7
anshusaurav <anshu.saurav@gmail.com>
2020-06-01 13:17:15 +0530

    Add addresses and list addresses view and route

--- routes/addresses.js
+++ routes/addresses.js
@@ -8,11 +8,73 @@ var Address = require('../models/address');
 router.get('/', async(req, res, next) =>{
     var id = req.session.userId;
     try{
-        var addresses = Address.find({user: id});
+        var addresses = await Address.find({user: id});
+        
+        console.log('Please');
         res.render('addresses', {addresses});
     } catch(error) {
         return next(error);
     }
 });
+router.get('/add', (req, res, next) =>{
+    console.log('new address');
+    res.render('newAddress');
+});
+router.post('/add', async(req, res, next) =>{
+    var id = req.session.userId;
+    req.body.user = id;
+    try{
+        var address = await Address.create(req.body);
+        var user = await User.findByIdAndUpdate(id, {$addToSet: {addresses: address.id}});
+        console.log(address, user);
+        res.redirect('/addresses');
+    }catch(error){
 
+    }
+});
+router.get('/:id/edit', async(req, res, next) =>{
+    var id = req.session.userId;
+    var addrId = req.params.id;
+
+    try{
+        var address = await Address.findById(addrId);
+        res.render('updateAddress', {address});
+    }catch(error){
+
+    }
+});
+
+router.post('/:id/edit', async(req, res, next) =>{
+    var id = req.session.userId;
+    req.body.user = id;
+    try{
+        var address = await Address.findByIdAndUpdate(id,req.body);
+        res.redirect('/addresses');
+    }catch(error){
+
+    }
+});
+
+router.get('/:id/delete', async(req, res, next) =>{
+    var id = req.session.userId;
+    req.body.user = id;
+    try{
+        var address = await Address.create(req.body);
+        var user = await User.findByIdAndUpdate(id, {$addToSet: {addresses: address.id}});
+        res.redirect('/addresses');
+    }catch(error){
+
+    }
+});
+router.get('/:id/makedefault', async(req, res, next) =>{
+    var id = req.session.userId;
+    req.body.user = id;
+    try{
+        var address = await Address.create(req.body);
+        var user = await User.findByIdAndUpdate(id, {$addToSet: {addresses: address.id}});
+        res.redirect('/addresses');
+    }catch(error){
+
+    }
+});
 module.exports = router;
